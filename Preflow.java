bd032c5a1a248fdef8bbee3818d9b78758bbcbcc
zacharias-selimovic <zacharias2002@live.se>
2024-11-22 15:34:28 +0100

    Starting read input, gotta go

--- Preflow.java
+++ Preflow.java
@@ -1,8 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Preflow{
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         
+        File graphFile = new File("/home/magizache/preflow/CPU/large_graph.edgelist");
+        Scanner scanner = new Scanner(graphFile);
+
+        
+        while (scanner.hasNextLine()) {
+            String line =  scanner.nextLine();
+
+        }
     }
 }
